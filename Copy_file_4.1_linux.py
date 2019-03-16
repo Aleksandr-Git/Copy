e00cfce86296bf9739e9e582ff05cf967f29edd6
Aleksandr-Git <gavryukov@mail.ru>
2019-03-16 19:25:56 +0000

    Копирование на яндекс диск

--- Copy_file_4.1_linux.py
+++ Copy_file_4.1_linux.py
@@ -1,11 +1,16 @@
+# работает
+# копирует на яндекс диск, перед запуском проверить примонтирован ли диск
+# перенести копирование в отдельный поток
+# потестировать на inux
+# 
+
 import os
 import shutil
 import time
+from threading import Thread
 
-path = '.\\pap_2'
-path_work = ''
-path_origin = ''
-path_copy = ''
+path = './pap_2'
+status_copy = False
 
 size = os.path.getsize(path)
 #files = os.listdir(path)
@@ -14,34 +19,25 @@ total_size = 0
 def fun(path):   # определяет размер папки
     global total_size
     for i in os.listdir(path):
-        if os.path.isdir(path + '\\' + i):
+        if os.path.isdir(path + '/' + i):
 
-            fun(path + '\\' + i)
+            fun(path + '/' + i)
 
         else:
-            s = os.path.getsize(path + '\\' + i)
+            s = os.path.getsize(path + '/' + i)
             total_size += s
     return total_size / 1024 / 1024
 
 #fun(path)
 print(str(total_size) + ' Мб')
 
-
 #fun_copy(path)
 
 cur_dir = os.getcwd()
 print(cur_dir)
 
-print(os.listdir('.\\pap_2'))
+print(os.listdir('./pap_2'))
 
-'''
-def copy_folder():
-    shutil.move('./pap_2' + '/' + os.listdir('./pap_2')[0], './pap_3')  # перемещает файлы из первой папки во вторую и удаляет первую
-    #os.rename('./pap_3/0', './pap_3/' + str(int(os.listdir('./pap_3')[-1]) + 1))
-    os.rename('./pap_3/0', './pap_3/' + str(int(len(os.listdir('./pap_3')))))
-    for i in os.listdir('./pap_2'):
-        os.rename('./pap_2' + '/' + i, './pap_2' + '/' + str(int(i) - 1))
-'''
 
 def last_pap():
     dir_list = [os.path.join(path, x) for x in os.listdir(path)]
@@ -54,26 +50,30 @@ def last_pap():
         sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=False)
 
         return sort_date_list[0][0]
-
-    
+  
 
 def copy_folder():
-    shutil.move(last_pap(), '.\\pap_3')
+    global status_copy
+#    shutil.move(last_pap(), './pap_3')
+    shutil.move(last_pap(), '/mnt/yandex_disk/pap_3')
+    status_copy = False
 
-print(fun('.\\pap_2'))
+print(fun('./pap_2'))
 print(last_pap())
 
 
 while True:
     total_size = 0
 
-    if fun('.\\pap_2') > 7:
+    if fun('./pap_2') > 20 and status_copy == False:
+        status_copy == True
 #        time.sleep(5)
 #        try:
-        copy_folder()
+        Thread(target = copy_folder())
+#        copy_folder()
 
 #        except Exception:
 #            continue
         print(str(total_size / 1024 / 1024) + ' Мб')
-        print(os.listdir('.\\pap_2'))
+        print(os.listdir('./pap_2'))
 
